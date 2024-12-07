eba39cc579f308838eecc6fe3d5f1cb04ac9918a
Lucas Shanks <lucassshanks@gmail.com>
2024-12-07 03:52:38 -0500

    nit

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -34,10 +34,7 @@
   .app {
     display: flex;
     flex-direction: column;
-    /* padding: 1rem 1rem; */
     min-height: 100vh;
-    /* max-width: 1000px; */
-    /* margin: 0 auto; */
     background-color: var(--color-light100);
   }
 
@@ -45,10 +42,6 @@
     flex: 1;
     display: flex;
     flex-direction: column;
-    /* padding: 1rem;
-		width: 100%;
-		max-width: 64rem;
-		margin: 0 auto; */
     box-sizing: border-box;
   }
 </style>
