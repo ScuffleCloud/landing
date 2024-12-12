c59a44b5795ae580e28446d0352826b21d5d8764
Lucas Shanks <lucassshanks@gmail.com>
2024-12-12 04:53:27 -0500

    push roadmap page

---
+++ src/lib/components/RoadmapPanel.svelte
@@ -0,0 +1,122 @@
+<script lang="ts">
+  import plannedIcon from '$lib/images/roadmap-dot-planned.svg';
+  import inProgressIcon from '$lib/images/roadmap-dot-in-progress.svg';
+  import completedIcon from '$lib/images/roadmap-dot-completed.svg';
+
+  export let title: string;
+  export let year: string;
+  export let content: {
+    title: string;
+    description: string;
+  }[] = [];
+  export let status: 'planned' | 'in-progress' | 'completed';
+  export let showDivider: boolean = true;
+
+  const statusIcons = {
+    planned: plannedIcon,
+    'in-progress': inProgressIcon,
+    completed: completedIcon,
+  };
+</script>
+
+<div class="panel">
+  <div class="left-section">
+    <h2>{title}</h2>
+    <p class="year">{year}</p>
+  </div>
+  <div class="divider-section">
+    <img src={statusIcons[status]} alt={status} />
+    {#if showDivider}
+      <div class="divider-line" class:planned={status === 'planned'}></div>
+    {/if}
+  </div>
+  <div class="right-section">
+    {#each content as item}
+      <div class="content-card">
+        <h3>{item.title}</h3>
+        <p>{item.description}</p>
+      </div>
+    {/each}
+  </div>
+</div>
+
+<style>
+  .panel {
+    display: grid;
+    grid-template-columns: 10rem 1px 1fr;
+    gap: 4rem;
+    width: 100%;
+  }
+
+  .left-section {
+    display: flex;
+    flex-direction: column;
+    gap: 0.5rem;
+
+    h2 {
+      font-size: 1.5rem;
+      font-weight: 700;
+      line-height: 2rem;
+    }
+
+    .year {
+      font-size: 1.125rem;
+      color: var(--color-brown700);
+    }
+  }
+
+  .divider-section {
+    margin-top: 0.125rem;
+    img {
+      transform: translateX(-50%);
+    }
+
+    .divider-line {
+      margin-top: 0.125rem;
+      background-color: var(--color-orange500);
+      width: 0.25rem;
+      transform: translateX(-50%);
+      height: 86%;
+      border-radius: 0.75rem;
+    }
+
+    .divider-line.planned {
+      background-color: var(--color-sand600);
+    }
+  }
+
+  .right-section {
+    display: flex;
+    flex-direction: column;
+    gap: 0.5rem;
+  }
+
+  .content-card {
+    display: flex;
+    flex-direction: column;
+    gap: 0.5rem;
+    padding: 1.5rem;
+    h3 {
+      font-size: 1.5rem;
+      font-weight: 700;
+    }
+
+    p {
+      font-size: 1.125rem;
+      line-height: 1.75rem;
+      font-weight: 500;
+    }
+  }
+
+  /* Maybe we want this to be on the left side when the page is small. TODO: Check with design */
+  @media (max-width: 768px) {
+    .panel {
+      grid-template-columns: 1fr;
+      gap: 1.5rem;
+    }
+
+    .divider-section {
+      display: none;
+    }
+  }
+</style>
--- src/routes/roadmap/+page.svelte
+++ src/routes/roadmap/+page.svelte
@@ -3,23 +3,32 @@
   import roadmap from '$lib/images/roadmap.svg';
   import { theme } from '$lib/theme';
   import backButton from '$lib/images/back-button.svg';
+  import RoadmapTimeline from '$lib/components/RoadmapTimeline.svelte';
+  import botToLaunch from '$lib/images/bot-to-launch.svg';
 </script>
 
 <div class="page-bg">
-  <div class="roadmap-container">
+  <div class="roadmap-header-container">
     <img src={roadmap} alt="Scuffle Roadmap" />
     <div class="header-overlay">
-      <h1>Scuffle's Roadmap</h1>
-      <p>
-        Here is an overview of the features we are currently<br />
-        developing, along with the progress made so far.<br />
-      </p>
-      <Pill color={theme.colors.orange500}>
-        <p>Detailed Kanban Board</p>
-        <img src={backButton} alt="Back" />
-      </Pill>
+      <div class="header-overlay-content">
+        <h1>Scuffle's Roadmap</h1>
+        <p>
+          Here is an overview of the features we are currently<br />
+          developing, along with the progress made so far.<br />
+        </p>
+      </div>
+      <a href="https://jira.scuffle.cloud/" class="pill-link">
+        <Pill color={theme.colors.orange500}>
+          <p>Detailed Kanban Board</p>
+          <img src={backButton} alt="Back" />
+        </Pill>
+      </a>
     </div>
   </div>
+  <RoadmapTimeline />
+  <!-- Now everytime i click this image. i want it to rise in the air -->
+  <img src={botToLaunch} alt="launch-bot" />
 </div>
 
 <style>
@@ -30,7 +39,7 @@
     padding: 1rem;
     width: 100%;
 
-    .roadmap-container {
+    .roadmap-header-container {
       margin-top: 1rem;
       display: flex;
       justify-content: center;
@@ -45,32 +54,51 @@
 
       .header-overlay {
         position: absolute;
-        text-align: center;
-        align-items: center;
         display: flex;
         flex-direction: column;
+        align-items: center;
+        text-align: center;
         width: 100%;
-        gap: 1rem;
         top: 24%;
+        gap: 1.5rem;
 
-        h1 {
-          font-size: clamp(1.5rem, 4.6vw, 3rem);
+        .header-overlay-content {
+          display: flex;
+          flex-direction: column;
+          align-items: center;
+          gap: 0.5rem;
+
+          h1 {
+            font-size: clamp(1.5rem, 4.6vw, 4rem);
+          }
+
+          p {
+            font-size: clamp(0.75rem, 2.3vw, 1.5rem);
+            line-height: clamp(1.2rem, 3.7vw, 2rem);
+            font-weight: 400;
+          }
         }
 
-        p {
-          font-size: clamp(0.875rem, 2.3vw, 1.5rem);
+        /* TODO: Touch up clamp values when font-sizes and other designs are finalized */
+        :global(.pill) {
+          font-weight: 700;
+          height: clamp(2rem, 6vw, 3rem);
+          gap: 0.25rem;
+          white-space: nowrap;
+          padding: 0 clamp(0.75rem, 2vw, 1rem);
+
+          img {
+            width: clamp(1rem, 3vw, 1.5rem);
+            height: auto;
+          }
+
+          p {
+            font-size: clamp(0.75rem, 2.3vw, 1rem);
+          }
         }
-      }
 
-      /* TODO: Make this responsive */
-      :global(.pill) {
-        font-weight: 700;
-        height: 3rem;
-        gap: 0.25rem;
-        white-space: nowrap;
-
-        p {
-          font-size: 1rem;
+        .pill-link {
+          text-decoration: none;
         }
       }
     }
