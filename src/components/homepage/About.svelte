45344d4a75ce7ec63318283a4737f5d6f1dfbd84
Lucas Shanks <lucassshanks@gmail.com>
2025-02-12 17:56:01 -0500

    fix troy nit

--- src/components/homepage/About.svelte
+++ src/components/homepage/About.svelte
@@ -54,14 +54,6 @@
             justify-content: center;
             align-items: center;
             grid-area: 1 / 1 / 3 / 2;
-
-            :global(svg) {
-                width: 100%;
-                height: 100%;
-                max-width: 100%;
-                max-height: 100%;
-                object-fit: contain;
-            }
         }
 
         .content-2 {
